304ad912c135f8ecc9bd20bb33ca93ca8dffb2c2
Andy Yu <andyyu2004@gmail.com>
2020-06-12 03:09:42 +1200

    store buffer on heap; impl parse response

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,16 +1,18 @@
 use crate::connection::Connection;
-use mp_protocol::{ProtocolResult, Request, BUF_CAP};
+use mp_protocol::{ProtocolResult, Request, Response, BUF_CAP};
 use std::path::Path;
 
 impl Connection {
-    pub(crate) async fn add_files(&mut self, files: Vec<&Path>) -> ProtocolResult<()> {
-        let mut buf = [0u8; BUF_CAP];
+    pub(crate) async fn add_files(&mut self, files: Vec<&Path>) -> ProtocolResult<Response> {
+        let mut buf = vec![0u8; BUF_CAP];
         let req = Request::AddFile(files);
         let count = mp_protocol::binary_encode_to_bytes(&req, &mut buf[..])?;
         self.send(&buf[..count]).await?;
 
         let count = self.recv(&mut buf).await?;
         println!("recv: {:x?}", &buf[..count]);
-        Ok(())
+        let response = serde_json::from_slice::<Response>(&buf[..count])?;
+        dbg!(&response);
+        Ok(response)
     }
 }
